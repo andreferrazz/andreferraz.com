69982de404939ea8769dd6eb618cc5bc959a33f7
Andre Ferraz <andreferraz.dv@gmail.com>
2020-11-02 21:08:31 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const btnIcon = document.getElementById('menu-mobile-icon');
+const menuLinks = document.getElementById('links');
+
+let menuIsOpen = true;
+
+const toggleMenu = () => {
+  console.log(menuLinks);
+  if (menuIsOpen) {
+    console.log('close');
+    btnIcon.classList.remove('fa-times');
+    btnIcon.classList.add('fa-bars');
+    menuLinks.style.display = 'none';
+
+    menuIsOpen = false;
+  } else {
+    console.log('open');
+    btnIcon.classList.remove('fa-bars');
+    btnIcon.classList.add('fa-times');
+    menuLinks.style.display = 'flex';
+
+    menuIsOpen = true;
+  }
+}
