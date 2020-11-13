bfd3a5cddc43efa30fe758b349416496be2d74c1
André Ferraz <andreferraz25384@gmail.com>
2020-11-12 22:17:43 -0300

    Remove some garbage from script

--- js/script.js
+++ js/script.js
@@ -3,26 +3,21 @@ const menuLinks = document.getElementById('links');
 
 let menuIsOpen = false;
 
-let example = {
-  some: 'lsdfklsadlfj',
-  other: 'ssakjdh',
-}
-
 const toggleMenu = () => {
-  console.log(menuLinks);
+
   if (menuIsOpen) {
-    console.log('close');
+
     btnIcon.classList.remove('fa-times');
     btnIcon.classList.add('fa-bars');
     menuLinks.style.display = 'none';
-
     menuIsOpen = false;
+
   } else {
-    console.log('open');
+
     btnIcon.classList.remove('fa-bars');
     btnIcon.classList.add('fa-times');
     menuLinks.style.display = 'flex';
-
     menuIsOpen = true;
+
   }
 }
