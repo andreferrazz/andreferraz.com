5928b09c4452a1a2caf7537af7fb5b255dd7d6e6
Andre Ferraz <andreferraz25384@gmail.com>
2021-10-29 21:15:16 -0300

    Change header style to improve readability.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-const btnIcon = document.getElementById('menu-mobile-icon');
-const menuLinks = document.getElementById('links');
+const header = document.getElementById('header')
+const logo = document.getElementById('logo')
+const navLinks = document.querySelectorAll('.nav-link')
 
-let menuIsOpen = false;
-
-const toggleMenu = () => {
-
-  if (menuIsOpen) {
-
-    btnIcon.classList.remove('fa-times');
-    btnIcon.classList.add('fa-bars');
-    menuLinks.style.display = 'none';
-    menuIsOpen = false;
-
-  } else {
-
-    btnIcon.classList.remove('fa-bars');
-    btnIcon.classList.add('fa-times');
-    menuLinks.style.display = 'flex';
-    menuIsOpen = true;
-
-  }
+function setHeaderDark() {
+  header.style.backgroundColor = '#272727'
+  logo.style.color = 'white'
+  navLinks.forEach(navLink => navLink.style.color = 'white')
 }
+
+function setHeaderLight() {
+  header.style.backgroundColor = 'white'
+  logo.style.color = '#272727'
+  navLinks.forEach(navLink => navLink.style.color = '#272727')
+}
+
+window.scrollY === 0 ? setHeaderDark() : setHeaderLight()
+
+window.addEventListener(
+  "scroll",
+  () => {
+    if (window.scrollY > 200) return
+
+    window.scrollY < 100 ? setHeaderDark() : setHeaderLight()
+  },
+  { passive: true }
+);
