9f0c142a38e40e5328ec7c98ba147183b341416b
andreferrazz <oandreferraz@gmail.com>
2024-11-14 19:32:33 -0300

    Create home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,21 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="fixed h-[50px] w-full border-b border-solid border-slate-300">
+	<div class="container mx-auto flex h-[50px] max-w-4xl items-center justify-between px-4">
+		<a href="/" class="flex items-center gap-1">
+			<img src="gui-terminal.svg" alt="GUI Terminal" class="w-[30px]" />
+			<div class="hidden font-bold sm:block">André Ferraz</div>
+		</a>
+
+		<div>
+			<a class="mx-1 px-3 py-1 text-sm font-bold" href="/blog">Articles</a>
+			<a class="mx-1 px-3 py-1 text-sm font-bold" href="/projects">Projects</a>
+			<a class="mx-1 px-3 py-1 text-sm font-bold" href="/about">About Me</a>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto max-w-4xl px-4">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section
+	class="flex h-full flex-col-reverse items-start gap-8 pt-[100px] sm:pb-[120px] pb-[80px] sm:flex-row sm:items-center sm:pt-[200px]"
+>
+	<div class="sm:w-2/3">
+		<h1 class="text-5xl font-bold">Hey, I'm André</h1>
+		<p class="mt-8 text-lg">
+			I'm a software engineer passionate about building scalable solutions and simplifying complex
+			systems. Here, you can explore my technical insights, learn more about my journey, and check
+			out projects and stories that shaped my career.
+		</p>
+	</div>
+	<div class="w-[200px] sm:w-1/3">
+		<img src="ram.png" alt="RAM Ram" />
+	</div>
+</section>
+
+<section>
+	<div class="flex justify-between">
+		<h2 class="text-3xl font-bold">Articles</h2>
+		<a href="/blog">All Articles</a>
+	</div>
+	<div>
+		<a href="/article1">
+			<div class="flex justify-between">
+				<div>Article 1</div>
+				<div>October 2024</div>
+			</div>
+		</a>
+		<a href="/article2">
+			<div class="flex justify-between">
+				<div>Article 2</div>
+				<div>August 2024</div>
+			</div>
+		</a>
+		<a href="/article3">
+			<div class="flex justify-between">
+				<div>Article 3</div>
+				<div>July 2024</div>
+			</div>
+		</a>
+	</div>
+</section>
