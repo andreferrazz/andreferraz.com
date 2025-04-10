c7bf422e9527de41d30397c3aea9c46d2afcdb05
André Ferraz <oandreferraz@gmail.com>
2025-04-10 16:56:28 -0300

    Personal website 2.0

--- src/lib/components/nav-link.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	let { text, href } = $props();
-</script>
-
-<a {href} class="mx-1 rounded-xl border-solid border-gruvbox-bg1 px-2 py-2 text-sm font-normal active:border active:bg-gruvbox-bg0-hard1 sm:px-4">
-	{text}
-</a>
--- src/lib/components/section-title.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	let { text } = $props();
-</script>
-
-<h2 class="text-2xl font-medium sm:text-3xl">{text}</h2>
